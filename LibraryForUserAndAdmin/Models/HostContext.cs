9002c60ef02cd01884747dc81a7654737d6e42c5
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-10-24 17:24:21 +0300

    showConnStr

--- LibraryForUserAndAdmin/Models/HostContext.cs
+++ LibraryForUserAndAdmin/Models/HostContext.cs
@@ -8,12 +8,12 @@ namespace PZ_ASP_UnitOfWorks.Models
         public DbSet<Not> Nots { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            new ConfigurationBuilder().AddUserSecrets<HostContext>()
-                .Build()
-                .Providers
-                .First()
-                .TryGet("connStr", out var connStr);
-            //string connStr = "Data Source=SQL5105.site4now.net;Initial Catalog=db_a8dfed_mydatabase1;User Id=db_a8dfed_mydatabase1_admin;Password=2657sgnusmas";
+            //new ConfigurationBuilder().AddUserSecrets<HostContext>()
+            //    .Build()
+            //    .Providers
+            //    .First()
+            //    .TryGet("connStr", out var connStr);
+            string connStr = "Data Source=SQL5105.site4now.net;Initial Catalog=db_a8dfed_mydatabase1;User Id=db_a8dfed_mydatabase1_admin;Password=2657sgnusmas";
             builder.UseSqlServer(connStr);
         }
     }
